0f103a69d1e988461f8b0f26197d84053eb290fe
Ulfred Chan <101036368+undarr@users.noreply.github.com>
2026-07-08 03:13:57 +0800

    Add Selenium WebDriver imports to app.py
    Added necessary imports for Selenium WebDriver functionality.

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from webdriver_manager.firefox import GeckoDriverManager
 
 def get_driver():
